fd2bc8e18532d589c6deafb2c4a644056ca2063b
mlarboul <mlarboul@student.42.fr>
2021-04-27 10:20:34 +0200

    adding strcpy test in main

--- test/main.c
+++ test/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-int	_ft_strlen(char *str);
+int		ft_strlen(char *str);
+char	*ft_strcpy(char *dst, char *src);
 
-int main(int argc, char **argv)
+int main(void)
 {
-	int len = 0;
+	char	src[] = "Jean-Baptiste";
+	char	dst[14];
+	int		len = 0;
 
-	if (argc != 2)
-		return (0);
-	len = _ft_strlen(argv[1]);
-	printf("len :%d\n", len);
+	len = ft_strlen(src);
+	printf("//Test 1//\n");
+	printf("Lenght of \"%s\" is %d\n", src, len);
+	
+	printf("//Test 2//\n");
+	printf("Copy of \"%s\" is \"%s\"\n", src, ft_strcpy(dst, src));
 	return (0);
 }
