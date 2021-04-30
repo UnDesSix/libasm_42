701b45750e6b59afd3c1c845e317ac5626e3df14
mlarboul <mlarboul@student.42.fr>
2021-04-30 07:52:13 +0200

    adding read, write, strdup test in main

--- test/main.c
+++ test/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
 #include <string.h>
 
-int		ft_strlen(char *str);
-char	*ft_strcpy(char *dst, char *src);
-int		ft_strcmp(char *src1, char *src2);
+int		ft_strlen(const char *str);
+char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *src1, const char *src2);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, const void *buf, size_t count);
+char	*ft_strdup(const char *src);
 
 
 int main(void)
 {
 	char	src[] = "Hello World!"; // strlen : 12
+	char	buffer[100]; // of size 100
 	char	dst[14];
 
 //	char	*src0 = NULL;
@@ -22,6 +28,8 @@ int main(void)
 	char	src6[] = "\0";
 	char	src7[] = "Matt";
 	char	src8[] = "Matt";
+
+	int		fd = 0;
 
 	printf("//Test 1 : STRLEN//\n");
 	printf("Lenght of \"%s\" is %d\n", src, ft_strlen(src));
@@ -41,7 +49,27 @@ int main(void)
 	printf("REAL : %d\n", strcmp(src7, src8));
 
 	printf("\n//Test 4 : WRITE//\n");
-	printf("MINE : %d\n", ft_write(1, src, ft_strlen(src)));
-	printf("REAL : %d\n", write(1, src, strlen(src)));
+	printf("MINE :\n");
+	printf(" with a return value of : %zd\n", ft_write(1, src, 3));
+	printf(" and a errno of : %d\n", errno);
+	printf("REAL :\n");
+	printf(" with a return value of : %zd\n", write(1, src, 3));
+	printf(" and a errno of : %d\n", errno);
+
+	printf("\n//Test 5 : READ//\n");
+	fd = open("Makefile", O_RDONLY);
+	printf("MINE :\n");
+	printf(" with a return value of : %zd\n", ft_read(fd, buffer, 30));
+	printf(" and a errno of : %d\n", errno);
+//	printf(" content : %s\n", buffer);
+	printf("REAL :\n");
+	printf(" with a return value of : %zd\n", read(fd, buffer, 30));
+	printf(" and a errno of : %d\n", errno);
+//	printf(" content : %s\n", buffer);
+
+	printf("\n//Test 6 :STRDUP//\n");
+	printf("MINE : %s\n", ft_strdup("Matt"));
+	printf("REAL : %s\n", strdup("Matt"));
+
 	return (0);
 }
